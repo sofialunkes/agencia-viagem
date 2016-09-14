b29d64b7fdddbc89fd7f4d859d445642faeb1d4c
Sofia Lunkes <sofia.big@live.com>
2016-09-14 16:14:33 -0300

    Incluindo o projeto

---
+++ agencia-viagem/App_Code/Classe/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Hotel
+/// </summary>
+public class Hotel
+{
+    public Hotel()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
