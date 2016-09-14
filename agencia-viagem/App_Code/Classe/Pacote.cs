edd02efcb3bb4598417dd95815a8b95a1850b5c4
Aluno <Aluno>
2016-09-14 16:41:22 -0300

    add atributos class pacote

--- agencia-viagem/App_Code/Classe/Pacote.cs
+++ agencia-viagem/App_Code/Classe/Pacote.cs
@@ -8,5 +8,10 @@ using System.Web;
 /// </summary>
 public class Pacote
 {
-    
+    public int Codigo { get; set; }
+    public string Descricao { get; set; }
+    public DateTime DataInicio { get; set; }
+    public int QtdDias { get; set; }
+    public Hotel Hotel { get; set; }
+    public double Valor { get; set; }
 }
