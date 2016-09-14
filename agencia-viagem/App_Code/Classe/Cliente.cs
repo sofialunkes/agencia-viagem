0145c826add193a517f4d53fde68d995c1b0d81c
Aluno <Aluno>
2016-09-14 16:28:20 -0300

    add atributos class cliente e criando class pacote

---
+++ agencia-viagem/App_Code/Classe/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Clientes
+/// </summary>
+public class Cliente
+{
+    public int Codigo { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Telefone { get; set; }    
+}
--- agencia-viagem/App_Code/Classe/Pacote.cs
+++ agencia-viagem/App_Code/Classe/Pacote.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-public class Hotel
+/// <summary>
+/// Summary description for Pacote
+/// </summary>
+public class Pacote
 {
-    public Hotel()
-    {
-
-    }
+    
 }
