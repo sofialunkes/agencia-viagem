3f9c89f99583d90a2b04c30f1fd27eb47dcbb2e7
Aluno <Aluno>
2016-09-14 16:44:56 -0300

    add persistencia clienteBD e pacoteBD

---
+++ agencia-viagem/App_Code/Persistencia/ClienteBD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClienteBD
+/// </summary>
+public class ClienteBD
+{
+	public ClienteBD()
+	{
+
+	}
+}
