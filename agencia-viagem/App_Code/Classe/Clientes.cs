462413c92dfd44601529660477345542adb7418c
Aluno <Aluno>
2016-09-14 16:18:38 -0300

    add class cliente

---
+++ agencia-viagem/App_Code/Classe/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Clientes
+/// </summary>
+public class Clientes
+{
+	public Clientes()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
--- agencia-viagem/App_Code/Classe/Hotel.cs
+++ agencia-viagem/App_Code/Classe/Hotel.cs
@@ -10,8 +10,6 @@ public class Hotel
 {
     public Hotel()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+
     }
 }
