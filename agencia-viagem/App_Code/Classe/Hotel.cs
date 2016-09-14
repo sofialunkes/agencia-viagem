bdb235008bbab1bd9fe9af00d2048833b8c97641
Sofia Lunkes <sofia.big@live.com>
2016-09-14 16:17:56 -0300

    adicionando o hotel

--- agencia-viagem/App_Code/Classe/Hotel.cs
+++ agencia-viagem/App_Code/Classe/Hotel.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for Hotel
-/// </summary>
 public class Hotel
 {
     public Hotel()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        
     }
 }
